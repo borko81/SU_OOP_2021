21a86df28a1662a6afa38d5382d68236284b171e
Borko <bstoilov81@gmail.com>
2021-03-15 13:26:01 +0200

    LSP

--- SOLID LAB/03_LSP/robots.py
+++ SOLID LAB/03_LSP/robots.py
@@ -1,3 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class Sensors(ABC):
+    @abstractmethod
+    def get_sensors(self):
+        pass
+
+
 class Robot:
     def __init__(self, type):
         self.type = type
@@ -6,22 +15,19 @@ class Robot:
         return self.type
 
 
-class Android(Robot):
-    def android_senzors_count(self):
+class Android(Robot, Sensors):
+    def get_sensors(self):
         return 4
 
 
 class Chappie(Robot):
-    def chappie_senzors_count(self):
+    def get_sensors(self):
         return 6
 
 
 def count_robot_senzors(robots: list):
     for robot in robots:
-        if isinstance(robot, Android):
-            print(robot.android_senzors_count())
-        elif isinstance(robot, Chappie):
-            print(robot.chappie_senzors_count())
+        print(robot.get_sensors())
 
 
 robots = [Android('Robocop'), Chappie('XIX')]
