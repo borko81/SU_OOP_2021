24d3e94b9c4f58dff762f4160d747e3fab4f678f
Borko <bstoilov81@gmail.com>
2021-03-15 13:45:18 +0200

    ISP

--- SOLID LAB/04_ISP/entertainment_system.py
+++ SOLID LAB/04_ISP/entertainment_system.py
@@ -1,46 +1,66 @@
-class EntertainmentDevice:
-    def connect_to_device_via_hdmi_cable(self, device): pass
-    def connect_to_device_via_rca_cable(self, device): pass
-    def connect_to_device_via_ethernet_cable(self, device): pass
-    def connect_device_to_power_outlet(self, device): pass
+# class EntertainmentDevice:
+#     def connect_to_device_via_hdmi_cable(self, device): pass
+#
+#     def connect_to_device_via_rca_cable(self, device): pass
+#
+#     def connect_to_device_via_ethernet_cable(self, device): pass
+#
+#     def connect_device_to_power_outlet(self, device): pass
+#
+
+class RcaConector:
+    def connect_to_device_via_rca_cable(self, device):
+        pass
 
 
-class Television(EntertainmentDevice):
-    def connect_to_dvd(self, dvd_player):
-        self.connect_to_device_via_rca_cable(dvd_player)
+class HdmiConnector:
+    def connect_to_device_via_hdmi_cable(self, device):
+        pass
+
+
+class EthernetConnector:
+    def connect_to_device_via_ethernet_cable(self, device):
+        pass
+
+
+class PowerConnector:
+    def connect_device_to_power_outlet(self, device):
+        pass
+
+
+class Television(RcaConector, HdmiConnector):
+    def connect_to_device_via_rca_cable(self, device):
+        pass
 
     def connect_to_game_console(self, game_console):
         self.connect_to_device_via_hdmi_cable(game_console)
 
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
+    def connect_to_device_via_hdmi_cable(self, device):
+        pass
 
 
-class dvd_player(EntertainmentDevice):
-    def connect_to_tv(self, television):
-        self.connect_to_device_via_hdmi_cable(television)
+class dvd_player(HdmiConnector, PowerConnector):
+    def connect_to_device_via_hdmi_cable(self, device):
+        pass
 
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
+    def connect_device_to_power_outlet(self, device):
+        pass
 
 
-class GameConsole(EntertainmentDevice):
-    def connect_to_tv(self, television):
-        self.connect_to_device_via_hdmi_cable(television)
+class GameConsole(Television, EthernetConnector):
+    def connect_to_device_via_rca_cable(self, device):
+        pass
 
     def connect_to_router(self, router):
         self.connect_to_device_via_ethernet_cable(router)
 
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
+    def connect_to_device_via_ethernet_cable(self, device):
+        pass
 
 
-class Router(EntertainmentDevice):
-    def connect_to_tv(self, television):
-        self.connect_to_device_via_ethernet_cable(television)
+class Router(EthernetConnector, PowerConnector):
+    def connect_to_device_via_ethernet_cable(self, device):
+        pass
 
-    def connect_to_game_console(self, game_console):
-        self.connect_to_device_via_ethernet_cable(game_console)
-
-    def plug_in_power(self):
-        self.connect_device_to_power_outlet(self)
+    def connect_device_to_power_outlet(self, device):
+        pass
